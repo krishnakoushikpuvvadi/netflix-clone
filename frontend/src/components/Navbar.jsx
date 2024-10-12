09ae3a03a32ae60f8b6cb37134b39de60bf42c67
Puvvadi Krishna Koushik <138456811+krishnakoushikpuvvadi@users.noreply.github.com>
2024-10-12 13:27:06 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -58,11 +58,16 @@ const Navbar = () => {
         <div className='w-full sm:hidden mt-4 z-50 bg-black border rounded border-gray-800'>
             <Link to ={'/'}
             className='block hover:underline p-2'
-            onClick = {toggleMobileMenu}
+            onClick={() => {
+              setContentType("movie");
+              toggleMobileMenu();
             >Movies</Link>
              <Link to ={'/'}
             className='block hover:underline p-2'
-            onClick = {toggleMobileMenu}
+            onClick={() => {
+              setContentType("tv");
+              toggleMobileMenu();
+          }}
             >Tv shows</Link>
              <Link to ={'/history'}
             className='block hover:underline p-2'
